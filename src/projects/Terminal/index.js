ffab05ba2f27a8a4e49057fe4f722a060b9851c6
michaelolson1996 <michaelolson.1996@protonmail.com>
2021-09-01 00:48:01 -0600

    adding functionality to terminal

--- src/projects/Terminal/index.js
+++ src/projects/Terminal/index.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import './terminal.css';
+import projects from './projects.json';
 
-const myTerminal = document.getElementById("terminal-textarea");
-
-const awaitUserInput = (evt) => {
-    console.log(evt.key)
+const awaitUserInput = (textarea) => {
+    // document.getElementById("lineNo").innerHTML = textarea.value.substr(0, textarea.selectionStart).split("\n").length;
+    
 }
 
 const getTab = (evt) => {
-    if (evt.key == "Tab") {
-        myTerminal.append("\n$ ")
-    } else if (evt.key == "Backspace") {
+    // console.log(evt.key)
+    if (evt.key === "Tab") {
+        document.getElementById("terminal-textarea").value += "\n$ ";
+    } else if (evt.key === "Backspace") {
         console.log("back")
+    } else if (evt.key === "Enter") {
+        let rowArr = document
+            .getElementById("terminal-textarea")
+            .value.split("\n$ ")
+        
+        let currentCmd = rowArr[rowArr.length - 1]
+        console.log(currentCmd)
+        // let commandCheck = /^$ ls/
+        // if ()
+        if (currentCmd === "$ " || currentCmd === "\n$ ") {
+            document.getElementById("terminal-textarea").value += "\n$ ";
+        }
     }
 }
 
 const Terminal = () => {
     return (
         <div id="main-wrapper">
-            <textarea defaultValue={"$ "} id="terminal-textarea" onKeyDown={getTab} onKeyUp={awaitUserInput}>
+            <div id="lineNo"></div>
+            <textarea autoFocus={true} defaultValue={"$ "} id="terminal-textarea" onKeyDown={getTab} onKeyUp={() => awaitUserInput(document.getElementById("terminal-textarea"))}>
                 
             </textarea>
         </div>
